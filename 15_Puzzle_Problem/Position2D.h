f961e8e9de780d4786f16f3476aa131e7b042cd9
13events <jdmartinez90@protonmail.com>
2018-09-06 15:47:00 -0700

    Created clearer pseudocode for move function, changed some objects to not use pointers Created new print method for Position2D and made other internal adjustments.

--- 15_Puzzle_Problem/Position2D.h
+++ 15_Puzzle_Problem/Position2D.h
@@ -1,10 +1,20 @@
 #include "pch.h"
+#include <iostream>
+#include "stdio.h"
+#include <sstream>
+//#include <string>
 
 //Structure used to hold x,y coordinates for gap position
 struct Position2D {
+private:
 	int x;
 	int y;
-
+public:
+	//default constructor, set all to 0
+	Position2D() {
+		this->x = 0;
+		this->y = 0;
+	}
 	//basic constructor
 	Position2D(int x, int y) {
 		this->x = x;
@@ -19,5 +29,17 @@ struct Position2D {
 	//returns y value
 	int getY(){
 		return this->y;
+	}
+
+	void setNewPosition(int x, int y) {
+		this->x = x;
+		this->y = y;
+
+	}
+	std::string print() {
+		
+		std::ostringstream oss;
+		oss << '(' << this->getX() << ',' << this->getY() << ')';
+		return oss.str();
 	}
 };
